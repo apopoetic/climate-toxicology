9c8e5c8a1a92d2f87ad73cda7c266daf01847053
Ankur Jamwal <ankurjamwal@live.com>
2022-07-17 16:48:21 +0530

    29-07-2022

--- Scripts.R
+++ Scripts.R
@@ -1,3 +1,6 @@
 #blogdown::serve_site() #use it one per session
 
 #blogdown::check_site() # when in doubt run this to find possible errors
+library(blogdown)
+new_site(theme = "yihui/hugo-xmag")
+y
